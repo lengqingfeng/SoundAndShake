c659c6d4a966a688edd728caf2851bfb07116a4c
lengqingfeng <lengshengren@lengshengrentekiiMac.local>
2013-10-22 12:44:24 +0800

    Initial Commit

---
+++ SoundAndShake/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SoundAndShake
+//
+//  Created by lengshengren on 13-10-22.
+//  Copyright (c) 2013年 lengshengren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SoundAndShake/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SoundAndShake
+//
+//  Created by lengshengren on 13-10-22.
+//  Copyright (c) 2013年 lengshengren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
